b9571c57dc3db05793d8a32f0d99cf1507bfe707
ChandlerNgo <chandlerngo00@gmail.com>
2022-08-18 14:53:36 -0500

    exercise

---
+++ Day 1 Refresher/exercise1.js
@@ -0,0 +1,12 @@
+const empty = [];
+const fiveelements = [1,2,3,4,5]
+console.log(fiveelements.length);
+console.log(fiveelements[0],fiveelements[2],fiveelements[4])
+const mixedDataTypes = [1,"asdf",true,["sdf"],{"asdf":"sdfds"},("sdfd")]
+console.log(mixedDataTypes,mixedDataTypes.length)
+const itCompanies = ["Facebook","Google","Microsoft","Apple","IBM","Oracle","Amazon"]
+console.log(itCompanies,itCompanies.length)
+console.log(itCompanies[0],itCompanies[3],itCompanies[6])
+for (let i = 0; i < itCompanies.length; i++){
+    console.log(itCompanies[i])
+}
